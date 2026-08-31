dc310d0e501b24645d70df262197e9b259c04815
TakashiKuniyasu <t-kuniyasu@gr.meitec.com>
2026-08-31 14:02:56 +0900

    さらに厳しい入力チェックに対応

--- src/StudyRecord.cpp
+++ src/StudyRecord.cpp
@@ -118,7 +118,7 @@ bool StudyRecord::checkDate(std::string date){
             check = false;
         }
     }
-    if(getline(ss, sbuf, '/' )){
+    if(!ss.eof()){
         std::cout << "入力が正しくありません" << std::endl;
         return false;
     }
