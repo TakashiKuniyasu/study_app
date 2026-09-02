20899e1fc54b907506cd29e104fc3c72f110e779
TakashiKuniyasu <t-kuniyasu@gr.meitec.com>
2026-09-02 15:31:11 +0900

    テスト追加第一弾

--- tests/StudyManagerTest.cpp
+++ tests/StudyManagerTest.cpp
@@ -10,4 +10,14 @@ int main(){
 
     test_manager.addRecord(content, date, minute);
     assert(test_manager.getRecordCount() == 1);
+
+    content = "Docker";
+    date = "2026/8/29";
+    minute = 80;
+
+    test_manager.addRecord(content, date, minute);
+    assert(test_manager.getRecordCount() == 2);
+
+    test_manager.deleteRecord(2);
+    assert(test_manager.getRecordCount() == 1);
 }
