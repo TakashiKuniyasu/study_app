1b05e6eb824a1c94dcaef21ed13810a0382fee40
TakashiKuniyasu <t-kuniyasu@gr.meitec.com>
2026-09-02 14:02:20 +0900

    ステージし損ねた変更を追加

--- include/StudyManager.hpp
+++ include/StudyManager.hpp
@@ -12,13 +12,13 @@ public:
     void showRecords() const;
     void showTotalMinutes() const;
     void deleteRecord(int);
-    void loadFromFile();
+    void loadFromFile(const std::string&);
     void searchContent(const std::string&);
     void editPrevData(int,
                       std::string&,
                       std::string&,
                       int&);
 private:
-    void saveToFile() const;
+    void saveToFile(const std::string&) const;
     int prevID=0;
 };
