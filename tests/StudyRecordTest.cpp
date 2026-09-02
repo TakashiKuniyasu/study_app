8a10331eeba8fe0d4e26ed5d27800b19a2c0236f
TakashiKuniyasu <t-kuniyasu@gr.meitec.com>
2026-09-02 12:45:05 +0900

    テスト追加

--- tests/StudyRecordTest.cpp
+++ tests/StudyRecordTest.cpp
@@ -3,6 +3,7 @@
 #include "StudyRecord.hpp"
 
 int main(){
+    // 日付チェック
     assert(StudyRecord::checkDate("2024/2/29") == true);
     assert(StudyRecord::checkDate("1999/1/1") == false);
     assert(StudyRecord::checkDate("2027/1/1") == false);
@@ -19,9 +20,31 @@ int main(){
     assert(StudyRecord::checkDate("2026/11/31") == false);
     assert(StudyRecord::checkDate("2026/12/31") == true);
 
+    // 学習時間チェック
     assert(StudyRecord::checkMinute(-1) == false);
     assert(StudyRecord::checkMinute(0) == false);
     assert(StudyRecord::checkMinute(1) == true);
+
+    //コンストラクタ＋getter
+    StudyRecord record(
+        1,
+        "C++",
+        "2026/8/28",
+        60
+    );
+
+    assert(record.getID() == 1);
+    assert(record.getContent() == "C++");
+    assert(record.getDate() == "2026/8/28");
+    assert(record.getMinutes() == 60);
+
+    //update
+    record.update("Docker","2026/8/29",80);
+
+    assert(record.getID() == 1);
+    assert(record.getContent() == "Docker");
+    assert(record.getDate() == "2026/8/29");
+    assert(record.getMinutes() == 80);
 
     std::cout << "全テスト成功" << std::endl;
 
